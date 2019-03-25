bada549d119485fa3b15901c19ac009de9b9fbd0
wenzheng.chen <wenzheng.chen@baozun.com>
2019-03-25 09:57:11 +0800

    测试

---
+++ src/main/java/com/cwzsmile/redis/ali/Swap.java
@@ -0,0 +1,19 @@
+package com.cwzsmile.redis.ali;
+
+/**
+ * Created by csh9016 on 2019/3/25.
+ */
+public class Swap {
+
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        swap(a, b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void swap(Integer a,Integer b) {
+        //交换a,b的值
+    }
+}
