0f1a1f7984004fbb5335e88c6c6a4928ab32c719
wenzheng.chen <wenzheng.chen@baozun.com>
2019-04-02 15:16:39 +0800

    arr测试属性

---
+++ src/main/java/com/cwzsmile/redis/jvm/ArrTest.java
@@ -0,0 +1,11 @@
+package com.cwzsmile.redis.jvm;
+
+/**
+ * Created by csh9016 on 2019/4/1.
+ */
+public class ArrTest {
+
+    public static void main(String[] args) {
+        int[] a = new int[4];
+    }
+}
